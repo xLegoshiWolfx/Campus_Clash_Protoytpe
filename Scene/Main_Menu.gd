1d8402c10ee23b960f4e7b514b862a2826178361
Lucas <lucas.kslian@gmail.com>
2021-11-09 00:04:52 -0600

    Volume slider
    Volume slider added.

--- Scene/Main_Menu.gd
+++ Scene/Main_Menu.gd
@@ -10,5 +10,12 @@ func _on_Button2_pressed():
 func _on_Quit_button_pressed():
 	get_tree().quit()
 
+func _on_Volume_button_pressed():
+	pass
+
 func _ready():
 	Audio1.play_music()
+
+
+func _on_VSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),value)
