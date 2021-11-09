50e9d90a88b12b19c1e3621e6a5f520f27ee6ba3
BenjaminHines <benrh99@yahoo.com>
2021-11-09 15:34:13 -0600

    Buttons subtract from coins and make buildings appear
    The buttons do stuff now!!!

--- Scene/Human Timer Scene.gd
+++ Scene/Human Timer Scene.gd
@@ -8,6 +8,10 @@ onready var _human_timer = get_node("Human Timer Node")
 
 onready var _AI_timer = get_node("AI Timer Node")
 
+onready var _Human_Birta_Building = get_node("/root/TileMap/Human Birta Building")
+
+onready var _AI_Birta_Building = get_node("/root/TileMap/AI Birta Building")
+
 func _ready():
 	_human_timer.set_wait_time(1)
 	_human_timer.start()
@@ -21,3 +25,15 @@ func _on_Human_Timer_Node_timeout():
 
 func _on_AI_Timer_Node_timeout():
 	AI_currency += 1
+
+
+func _on_HumanBuildingButton_pressed():
+	if (human_currency >= 5):
+		human_currency -= 5
+		_Human_Birta_Building.visible = true
+
+
+func _on_AIBuildingButton_pressed():
+	if (AI_currency >= 5):
+		AI_currency -= 5
+		_AI_Birta_Building.visible = true
