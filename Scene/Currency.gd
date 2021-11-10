fd275b8e916b2a31a54c78691e7aa9bd64f94bb2
BenjaminHines <benrh99@yahoo.com>
2021-11-09 18:09:49 -0600

    Units can now be bought
    Behold the absolute units!!!

--- Scene/Currency.gd
+++ Scene/Currency.gd
@@ -16,6 +16,14 @@ onready var _Human_Birta_Building = get_node("/root/TileMap/Human Birta Building
 
 onready var _AI_Birta_Building = get_node("/root/TileMap/AI Birta Building")
 
+onready var _human_unit_visible = get_node("/root/TileMap/Moving_unit/YSort/Unit")
+
+onready var _human_unit_visible2 = get_node("/root/TileMap/Moving_unit/YSort/Unit2")
+
+onready var _AI_unit_visible = get_node("/root/TileMap/Moving_unit/YSort/Unit3")
+
+onready var _AI_unit_visible2 = get_node("/root/TileMap/Moving_unit/YSort/Unit4")
+
 func _ready():
 	_human_timer.set_wait_time(1)
 	_human_timer.start()
@@ -43,3 +51,19 @@ func _on_AIBuildingButton_pressed():
 		AI_currency -= 5
 		_AI_Birta_Building.visible = true
 		AI_building_mult += 2
+
+
+func _on_HumanUnitButton_pressed():
+	if (human_currency >= 20):
+		human_currency -= 20
+		_human_unit_visible.visible = true
+		_human_unit_visible2.visible = true
+
+
+func _on_AIUnitButton_pressed():
+	if (AI_currency >= 20):
+		AI_currency -= 20
+		_AI_unit_visible.visible = true
+		_AI_unit_visible2.visible = true
+		
+		#NICE
