5ab616010d9f877895951a79b22533672e0a1628
BenjaminHines <benrh99@yahoo.com>
2021-11-09 19:14:54 -0600

    Added Win Condition and More Buildings

--- Scene/Currency.gd
+++ Scene/Currency.gd
@@ -14,7 +14,15 @@ onready var _AI_timer = get_node("AI Timer Node")
 
 onready var _Human_Birta_Building = get_node("/root/TileMap/Human Birta Building")
 
+onready var _Human_Birta_Building2 = get_node("/root/TileMap/Human Birta Building2")
+
+onready var _Human_Birta_Building3 = get_node("/root/TileMap/Human Birta Building3")
+
 onready var _AI_Birta_Building = get_node("/root/TileMap/AI Birta Building")
+
+onready var _AI_Birta_Building2 = get_node("/root/TileMap/AI Birta Building2")
+
+onready var _AI_Birta_Building3 = get_node("/root/TileMap/AI Birta Building3")
 
 onready var _human_unit_visible = get_node("/root/TileMap/Moving_unit/YSort/Unit")
 
@@ -23,6 +31,10 @@ onready var _human_unit_visible2 = get_node("/root/TileMap/Moving_unit/YSort/Uni
 onready var _AI_unit_visible = get_node("/root/TileMap/Moving_unit/YSort/Unit3")
 
 onready var _AI_unit_visible2 = get_node("/root/TileMap/Moving_unit/YSort/Unit4")
+
+onready var _human_win_message = get_node("/root/TileMap/Currency/Human Win")
+
+onready var _AI_win_message = get_node("/root/TileMap/Currency/AI Win")
 
 func _ready():
 	_human_timer.set_wait_time(1)
@@ -40,30 +52,60 @@ func _on_AI_Timer_Node_timeout():
 
 
 func _on_HumanBuildingButton_pressed():
-	if (human_currency >= 5):
+	if (human_currency >= 5 and human_currency < 25 
+		and human_building_mult != 0):
 		human_currency -= 5
 		_Human_Birta_Building.visible = true
 		human_building_mult += 2
-		
+	if (human_currency >= 25 and human_currency < 100 
+		and _Human_Birta_Building.visible == true and human_building_mult != 0):
+		human_currency -= 25
+		_Human_Birta_Building2.visible = true
+		human_building_mult += 2
+	if (human_currency >= 100 and _Human_Birta_Building.visible == true 
+		and _Human_Birta_Building2.visible == true 
+		and human_building_mult != 0):
+		human_currency -= 100
+		_Human_Birta_Building3.visible = true
+		human_building_mult += 2
+	if (_Human_Birta_Building.visible == true 
+		and _Human_Birta_Building2.visible == true
+		and _Human_Birta_Building3.visible == true):
+			human_building_mult = 0
+			AI_building_mult = 0
+			_human_win_message.visible = true
 
 func _on_AIBuildingButton_pressed():
-	if (AI_currency >= 5):
+	if (AI_currency >= 5 and AI_currency < 25 and AI_building_mult != 0):
 		AI_currency -= 5
 		_AI_Birta_Building.visible = true
 		AI_building_mult += 2
-
+	if (AI_currency >= 25 and AI_currency < 100 
+		and _AI_Birta_Building.visible == true and AI_building_mult != 0):
+		AI_currency -= 25
+		_AI_Birta_Building2.visible = true
+		AI_building_mult += 2
+	if (AI_currency >= 100 and _AI_Birta_Building.visible == true 
+		and _AI_Birta_Building2.visible == true and AI_building_mult != 0):
+		AI_currency -= 100
+		_AI_Birta_Building3.visible = true
+		AI_building_mult += 2
+	if (_AI_Birta_Building.visible == true 
+		and _AI_Birta_Building2.visible == true
+		and _AI_Birta_Building3.visible == true):
+			human_building_mult = 0
+			AI_building_mult = 0
+			_AI_win_message.visible = true
 
 func _on_HumanUnitButton_pressed():
-	if (human_currency >= 20):
+	if (human_currency >= 20 and human_building_mult != 0):
 		human_currency -= 20
 		_human_unit_visible.visible = true
 		_human_unit_visible2.visible = true
 
 
 func _on_AIUnitButton_pressed():
-	if (AI_currency >= 20):
+	if (AI_currency >= 20 and AI_building_mult != 0):
 		AI_currency -= 20
 		_AI_unit_visible.visible = true
 		_AI_unit_visible2.visible = true
-		
-		#NICE
