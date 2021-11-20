cc5239af807a174183d009407a3ee7b435071e7b
IanPCC <ian.mcpherson@students.pcci.edu>
2021-11-20 15:49:47 -0600

    Added in half of the building health bars
    Still need to implement the other half of the buildings and implement signals to change health.

--- Script/Building Health Bar.gd
+++ Script/Building Health Bar.gd
@@ -1,23 +1,23 @@
 extends Node2D
 
-# Preload the healthbar assets
+# Preload the images.
 var bar_red = preload("res://PNG/HealthBars/barHorizontal_red.png")
 var bar_green = preload("res://PNG/HealthBars/barHorizontal_green.png")
 var bar_yellow = preload("res://PNG/HealthBars/barHorizontal_yellow.png")
 
-onready var healthbar = $HealthBar
+onready var healthbar = $BHealthBar
 
 func _ready():
 	hide()
-	if get_parent() and get_parent().get("max_health"):
-		healthbar.max_value = get_parent().max_health
+	if get_parent() and get_parent().get("building_health"):
+		healthbar.max_value = get_parent().building_health
 	update_healthbar()
 	print(healthbar.value)
 
 func _process(delta):
 	global_rotation = 0
-
-# Update the unit's health bar to reflect their current health.
+	
+# Update the health bar to reflect the building's health.
 func update_healthbar(health_value=0):
 	healthbar.texture_progress = bar_green
 	if health_value != 0:
